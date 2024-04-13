1d118e3503a4205a2db44165e3a4f3fa5b04909b
KotlinType <andrey.dozmolin@mail.ru>
2024-04-13 17:10:07 +0700

    Fix
    Added a description of the functions

--- LeetCode-tasks/src/easy/LongestCommonPrefix.kt
+++ LeetCode-tasks/src/easy/LongestCommonPrefix.kt
@@ -1,5 +1,14 @@
 package easy
 
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example:
+ * Input: strs = ["flower","flow","flight"]
+ *
+ * Output: "fl"
+ */
 fun longestCommonPrefix(strs: Array<String>): String {
     var prefix = ""
     strs[0].forEachIndexed { index, value ->
